8c6ed6ee4a95d32745d8f30b008102b7035f75a2
longlin <zoulonglin@sharkgulf.com>
2020-12-17 10:35:06 +0800

    简单的http服务

--- practice/main.go
+++ practice/main.go
@@ -1,6 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
-func main() {
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
 
+func main() {
+	r:=gin.Default()
+	r.GET("/home", func(context *gin.Context) {
+		context.JSON(http.StatusOK,"pong")
+	})
+	err:=r.Run(":8080")
+	if err!=nil{
+		fmt.Println("gin run error=",err)
+		os.Exit(-1)
+	}
 }
